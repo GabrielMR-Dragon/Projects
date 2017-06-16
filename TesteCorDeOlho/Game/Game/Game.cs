55d7748d8f9152d34ae1694bfe59b30886f62685
Gabriel Marshall Rebello <GabrielMR-Dragon@hotmail.com>
2017-06-16 08:05:55 -0300

    Novo projeto - Teste cor de olhos

---
+++ TesteCorDeOlho/Game/Game/Game.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TesteCorDeOlho
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class World : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D eyes;
+
+        public World()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1600;
+            graphics.PreferredBackBufferHeight = 600;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            // TODO: use this.Content to load your game content here
+
+            eyes = Content.Load<Texture2D>("Sprites/eyes");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+
+            spriteBatch.Begin();
+
+            // TODO: Add your drawing code here
+            spriteBatch.Draw(eyes, new Vector2(0, 0), null, Color.Gold, 0.0f, Vector2.Zero, Vector2.One, SpriteEffects.None, 0f);
+
+            spriteBatch.Draw(eyes, new Vector2(eyes.Width, 0), null, Color.Cyan, 0.0f, Vector2.Zero, Vector2.One, SpriteEffects.None, 0f);
+
+            spriteBatch.Draw(eyes, new Vector2(eyes.Width * 2, 0), null, Color.Chocolate, 0.0f, Vector2.Zero, Vector2.One, SpriteEffects.None, 0f);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
